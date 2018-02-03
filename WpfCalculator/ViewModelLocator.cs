f3704807b84a26fb1aba8b75ca2bfc41e9538c84
Aaron Harris <aaron.harris@pioneer.com>
2018-02-03 10:03:33 -0600

    Add ViewModel locator

---
+++ WpfCalculator/ViewModelLocator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfCalculator.ViewModels;
+
+namespace WpfCalculator
+{
+    public class ViewModelLocator
+    {
+        private static CalculatorViewModel _calculatorViewModel = new CalculatorViewModel(); 
+
+        public static CalculatorViewModel CalculatorViewModel
+        {
+            get
+            {
+                return _calculatorViewModel;
+            }
+        }
+    }
+}
